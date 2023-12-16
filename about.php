d1336940e43f5ab6c247b47956442b959df74ea4
Nasu7972 <153987630+Nasu7972@users.noreply.github.com>
2023-12-16 15:51:34 +0530

    I have added this about file

---
+++ about.php
@@ -0,0 +1,353 @@
+<?php include 'header.php'; ?>
+<?php include 'navbar.php'; ?>
+
+<body>
+  <section class="section1 b_212529 pt-5 ">
+    <div class="container-fluid pt-5">
+      <div class="row">
+        <div class="col-xl-3 col-lg-2 col-md-2 col-sm-12 col-12 "></div>
+        <div class="col-xl-6 col-lg-8 col-md-8 col-sm-12 col-12 mt-5">
+          <div class="row align-items-center " data-aos="fade-down" data-aos-duration="2000">
+            <div class="col-xl-5 col-lg-6 col-md-6 col-sm-12 col-12 text-center">
+              <img src="assets/images/about/Rectangle.jpg" class="img-fluid rounded-4  abo_img" alt="not found">
+            </div>
+            <div class="col-xl-7  col-lg-6 col-md-6 ol-sm-12 col-12 ">
+              <div data-aos="fade-down" data-aos-duration="2000">
+                <p class="c_09aff4 fw-medium fs_18 mb-3">About</p>
+                <h1 class="c_white fw-bold fs_26 mb-5">We are creative, smart
+                  and hardworking people</h1>
+                <p class="c_gray fw-lighter">HEPL stands as a leading one-stop destination for all your business needs,
+                  empowering your journey with services that include HR and Financial Shared Services, Talent
+                  Acquisition, BPO/KPO Operations, trailblazing Software Development solutions, dynamic web
+                  applications, Advance digital marketing service, Remote support management, captivating UI/UX designs,
+                  and more. Additionally, we specialize in crafting immersive audio-visual content to meet your brand's
+                  marketing needs and offer valuable brand marketing assistance. At HEPL, we are devoted to elevating
+                  your business and guiding it toward unparalleled success.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-lg-2 col-md-2 col-sm-12 col-12"></div>
+      </div>
+    </div>
+  </section>
+  <section class="section2 p-3 b_212529 ">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xl-3 col-lg-2 col-md-1 col-sm-12 col-12"></div>
+        <div class="col-xl-6 col-lg-8 col-md-9 col-sm-12 col-12 py-5">
+          <div class="row pt-4">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 " data-aos="zoom-in" data-aos-duration="2000">
+              <div class=" text-center c_white target">
+                <!-- <i class="fa-solid fa-bullseye c_white fs_40 p-5 "></i> -->
+                <img src="assets/images/about/target.gif" class="img-fluid mb-3 " alt="">
+                <p class=" fw-bold  mt-2"> “Delivering Excellence, Embracing
+                  Diversity” </p>
+                <p class=" c_gray fw-lighter my-5 ps-xl-4 ps-lg-4 ps-md-2 ps-sm-0 p-0 ">
+                  Driven by our customer-centric philosophy and a commitment to unmatched quality, our simple and
+                  intelligent approach, harmonized with the power of diversity and equal opportunity, leads us to
+                  consistently deliver exceptional service quality and enhance customer delight.
+                </p>
+              </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" data-aos="zoom-in" data-aos-duration="2000">
+              <div class=" text-center c_white eye">
+                <!-- <i class="fa-regular fa-eye c_white fs_40 p-5"></i> -->
+                <img src="assets/images/about/eyeimg.gif" class="img-fluid " alt="">
+                <p class=" fw-bold  mt-2"> “Transforming Tomorrow, Empowering Success” </p>
+                <p class=" c_gray fw-lighter  my-5 ps-xl-4 ps-lg-4 ps-md-2 ps-sm-0 p-0">
+                  With a clear vision to be the global choice for business transformation, we offer distinctive and
+                  innovative solutions that blend automation, analytics, data security and digital proficiency. Our
+                  ultimate goal is to provide an unmatched value proposition to our clients that surpass expectations.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-lg-2 col-md-1 col-sm-12 col-12"></div>
+      </div>
+    </div>
+  </section>
+  <section class="section3 P-3 b_212529 pb-5">
+    <div class="container-fluid">
+      <div class="row py-4">
+        <div class="col-12 text-center">
+          <p class="c_09aff4 py-4 fw-medium fs_18 mb-3">OUR TEAMS</p>
+          <h2 class="c_white fw-bold fs_26 mb-5">Here's what you will get when
+            purchasing this service</h2>
+        </div>
+      </div>
+      <div class="row leader">
+        <div class="col-xl-12 col-lg-12 col-sm-12 col-md-12 col-12" data-aos="fade-down" data-aos-easing="linear"
+          data-aos-duration="1500">
+          <div id="container">
+            <div id="holder">
+              <div id="image">
+                <div class="img jobs"></div>
+                <div class="img zuck"></div>
+                <div class="img musk"></div>
+              </div>
+              <div id="right"></div>
+              <div id="text">
+                <div id="centerText">
+                  <p class="quote">
+                    and liberating online code editor for developers of any skill, and particularly empowering for
+                    people
+                    learning to code. We focus primarily on front-end languages like HTML, CSS, JavaScript, and
+                    preprocessing syntaxes that turn into those things.
+                    <br>
+                    <a id="linkedinLink" href="https://www.linkedin.com">
+                      <i class="fa-brands c_linkedin fa-linkedin c_09aff4 fs_40 mt-xl-4 mt-lg-4 mt-md-3 mt-sm-1 mt-0"></i>
+                    </a>
+                  </p>
+                  <p class="quote">
+                    We say social, because CodePen is a community. Most creations on CodePen are public and open source.
+                    They are living things that other people and the community can interact with, from a simple
+                    hearting,
+                    to leaving a comment, to forking and changing for their <br>  <a id="linkedinLink" href="https://www.linkedin.com">
+                      <i class="fa-brands c_linkedin fa-linkedin c_09aff4 fs_40 mt-xl-4 mt-lg-4 mt-md-3 mt-sm-1 mt-0"></i>
+                    </a>
+                  </p>
+                  <p class="quote">
+                    People use CodePen for all sorts of things: prototyping new ideas, reduced test cases for bugs,
+                    sending clients things to look at, evaluating potential hires, to finding inspiration. People also
+                    use
+                    CodePen as a sort of resume and portfolio, showing off their best design <br>  <a id="linkedinLink" href="https://www.linkedin.com">
+                      <i class="fa-brands c_linkedin fa-linkedin c_09aff4 fs_40 mt-xl-4 mt-lg-4 mt-md-3 mt-sm-1 mt-0"></i>
+                    </a>
+                  </p>
+                </div>
+                <nav>
+                  <a id="leftBtn" class="prev"><i class="fa fa-arrow-left"></i></a>
+                  <a id="rightBtn" class="next"><i class="fa fa-arrow-right"></i></a>
+                </nav>
+              </div>
+            </div>
+            <div id="bg">
+              <div class="slide jobs-bg"></div>
+              <div class="slide zuck-bg"></div>
+              <div class="slide musk-bg"></div>
+            </div>
+            <div id="nav-cirlces">
+              <div class="circle"></div>
+              <div class="circle"></div>
+              <div class="circle"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="section4 p-3  b_212529 pt-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-3 col-lg-4 col-md-12 col-sm-12 col-12"></div>
+        <div class="col-xl-6 col-lg-4 col-md-12 col-sm-12 col-12">
+          <div class=" text-center">
+            <p class="c_09aff4 fw-medium fs_18 mb-3">Life at HEPL</p>
+            <p class="c_white fw-bold fs_26 mb-5">More Than Work Where Passion
+              Flourishes, & People Thrive</p>
+          </div>
+        </div>
+        <div class="col-xl-3 col-lg-4 col-md-12 col-sm-12 col-12"></div>
+      </div>
+      <!-- Gallery -->
+      <div class="row">
+        <div class="col-xl-4 col-lg-4 col-md-12 mb-4 mb-lg-0">
+          <img src="assets/images/about/food.png" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
+          <img src="assets/images/about/people.png" class="w-100 shadow-1-strong rounded mb-4"
+            alt="Wintry Mountain Landscape" />
+        </div>
+        <div class="col-xl-4 col-lg-4 mb-4 mb-lg-0">
+          <img src="assets/images/about/flower.png" class="w-100 shadow-1-strong rounded mb-4"
+            alt="Mountains in the Clouds" />
+          <img src="assets/images/about/dj.png" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
+        </div>
+        <div class="col-xl-4  col-lg-4 mb-4 mb-lg-0">
+          <img src="assets/images/about/playground.png" class="w-100 shadow-1-strong rounded mb-4" alt="Waves at Sea" />
+          <img src="assets/images/about/lamp.png" class="w-100 shadow-1-strong rounded mb-4"
+            alt="Yosemite National Park" />
+        </div>
+      </div>
+      <!-- Gallery -->
+    </div>
+  </section>
+  <?php include 'footer.php'; ?>
+  <!--     JQuery     -->
+  <script src="js/vendors/jquery-3.6.1.min.js"></script>
+
+  <!--     appear     -->
+  <script src="js/vendors/appear.min.js"></script>
+
+  <!--     bootstrap     -->
+  <script src="js/vendors/bootstrap.bundle.min.js"></script>
+
+  <!--     countTo     -->
+  <script src="js/vendors/jquery.countTo.js"></script>
+
+  <!--     wow     -->
+  <script src="js/vendors/wow.min.js"></script>
+
+  <!--     swiper     -->
+  <script src="js/vendors/swiper-bundle.min.js"></script>
+
+  <!--     particles     -->
+  <script src="js/vendors/particles.min.js"></script>
+
+  <!--     Vanilla-tilt     -->
+  <script src="js/vendors/vanilla-tilt.min.js"></script>
+
+  <!--     isotope     -->
+  <script src="js/vendors/isotope-min.js"></script>
+
+  <!--     fancybox     -->
+  <script src="js/vendors/jquery.fancybox.min.js"></script>
+
+  <!--     main     -->
+  <script src="js/main.js"></script>
+
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script>
+    $(window).load(function () {
+      $(".circle")
+        .first()
+        .addClass("active");
+      $("#bg .slide")
+        .first()
+        .addClass("active");
+      $("#image .img")
+        .first()
+        .addClass("active");
+      $(".quote")
+        .first()
+        .addClass("active");
+
+      var animate = setInterval(function () {
+        var $this = $(".circle"),
+          currentActiveBubble = $("#nav-cirlces").find(".active"),
+          position = $("#nav-cirlces")
+            .children()
+            .index(currentActiveBubble),
+          num = $(".circle").length;
+
+        if (position < num - 1) {
+          $(".active")
+            .removeClass("active")
+            .next()
+            .addClass("active");
+        } else {
+          $(".circle")
+            .removeClass("active")
+            .first()
+            .addClass("active");
+          $("#bg .slide")
+            .removeClass("active")
+            .first()
+            .addClass("active");
+          $("#image .img")
+            .removeClass("active")
+            .first()
+            .addClass("active");
+          $(".quote")
+            .removeClass("active")
+            .first()
+            .addClass("active");
+        }
+        position++;
+      }, 4500);
+
+      $(".circle").click(function () {
+        clearInterval(animate);
+
+        var $this = $(this),
+          $siblings = $this.parent().children(),
+          position = $siblings.index($this);
+
+        $(".circle")
+          .removeClass("active")
+          .eq(position)
+          .addClass("active");
+        $("#bg .slide")
+          .removeClass("active")
+          .eq(position)
+          .addClass("active");
+        $("#image .img")
+          .removeClass("active")
+          .eq(position)
+          .addClass("active");
+        $(".quote")
+          .removeClass("active")
+          .eq(position)
+          .addClass("active");
+      });
+
+      $("#rightBtn , #leftBtn").click(function () {
+        clearInterval(animate);
+
+        var $this = $(this),
+          currentActiveBubble = $("#nav-cirlces").find(".active"),
+          position = $("#nav-cirlces")
+            .children()
+            .index(currentActiveBubble),
+          num = $(".circle").length;
+
+        if ($this.hasClass("next")) {
+          if (position < num - 1) {
+            $(".active")
+              .removeClass("active")
+              .next()
+              .addClass("active");
+          } else {
+            $(".circle")
+              .removeClass("active")
+              .first()
+              .addClass("active");
+            $("#bg .slide")
+              .removeClass("active")
+              .first()
+              .addClass("active");
+            $("#image .img")
+              .removeClass("active")
+              .first()
+              .addClass("active");
+            $(".quote")
+              .removeClass("active")
+              .first()
+              .addClass("active");
+          }
+        } else {
+          if (position === 0) {
+            $(".circle")
+              .removeClass("active")
+              .last()
+              .addClass("active");
+            $("#bg .slide")
+              .removeClass("active")
+              .last()
+              .addClass("active");
+            $("#image .img")
+              .removeClass("active")
+              .last()
+              .addClass("active");
+            $(".quote")
+              .removeClass("active")
+              .last()
+              .addClass("active");
+          } else {
+            $(".active")
+              .removeClass("active")
+              .prev()
+              .addClass("active");
+          }
+        }
+      });
+    });
+
+  </script>
+  <script>
+    AOS.init();
+  </script>
+</body>
+
+</html>
